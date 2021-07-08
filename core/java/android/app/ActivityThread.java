40b9bc537ebc4abecd252eb69286f7c79bccdba4
Danny Lin <danny@kdrag0n.dev>
2021-07-07 21:22:35 -0700

    graphics: Override system fonts with user-selected overlays
    Only using user-selected fonts for a subset of system apps leads to an
    inconsistent visual experience. While we've already applied them to the
    system's Material UI themes, many apps have hard-code references to
    sans-serif (whether it's through XML styles or Typeface.SANS_SERIF), so
    it is also necessary to override Typeface fonts at runtime.
    
    Changing overlays causes OverlayManagerService to schedule
    ApplicationInfo updates for affected apps, which forces a configuration
    change by incrementing Configuration#assetsSeq.
    
    TODO: figure out why not all apps get consistent configuration updates
    until manually relaunching activities
    
    Squashed:
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Mon, 24 Jul 2023 15:35:10 +0800
        Subject: graphics: override more system fonts
    
        * google mostly hardcodes everything to google-sans family (sometimes monospace), override those fonts as well
        * requires changing of vendor themes google-sans font alias
    
        test: apply patch, check gboard key buttons if font style was overriden upon system font change
    
        Change-Id: I04a5f11e86716b3aa4ed90d84f988760aa7bec2b
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Tue, 31 Dec 2024 13:26:25 +0800
        Subject: [PATCH] Skip overriding fonts on some packages
    
    Change-Id: If03c9f120a5110b39d1f0e11c0a7fc3fd52fae53
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    [cleanup and improve font override]
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -8329,6 +8329,7 @@ public final class ActivityThread extends ClientTransactionHandler
 
         // Preload fonts resources
         FontsContract.setApplicationContextForResources(appContext);
+        Typeface.updateDefaultFont(data.info.getResources());
         if (!Process.isIsolated()) {
             try {
                 final ApplicationInfo info =
