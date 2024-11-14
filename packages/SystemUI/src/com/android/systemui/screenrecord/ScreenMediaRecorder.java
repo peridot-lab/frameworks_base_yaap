ef570aef8a362d718c085d768beae1273fd4b9f6
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-14 10:30:55 +0800

    Notification: Switch to low ram policy for bitmaps
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -514,7 +514,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
      * Returns the required {@code Size} of the thumbnail.
      */
     private Size getRequiredThumbnailSize() {
-        boolean isLowRam = ActivityManager.isLowRamDeviceStatic();
+        boolean isLowRam = true;
         int thumbnailIconHeight = mContext.getResources().getDimensionPixelSize(isLowRam
                 ? com.android.internal.R.dimen.notification_big_picture_max_height_low_ram
                 : com.android.internal.R.dimen.notification_big_picture_max_height);
