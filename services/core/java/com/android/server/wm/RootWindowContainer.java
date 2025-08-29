bf371cf6ab0155244a4c63355eb23d09ed1e4c21
Riddle Hsu <riddlehsu@google.com>
2025-08-29 14:40:07 +0800

    Reduce blocking operation on display thread
    For example, thread A acquires wm lock 20ms and it posts a
    message to another thread B that also needs to enter wm lock.
    Then B will need to wait for the 20ms operation to finish.
    
    Currently the common cases are the message of task-layer-rank
    and idle-check. For rank, it can be done with deferred scope
    without posting a message. For idle-check, it can post a
    regular 10ms timeout rather than an immediate message if the
    activity is in a transition because when transition is finished,
    scheduleProcessStoppingAndFinishingActivitiesIfNeeded will also
    check idle.
    
    Bug: 422656135
    Test: Return to home from an app by pressing home key.
          Check trace about the timestamp of setProcessGroup.
    Change-Id: I9f455f1063ef04c049c5743eda9f6c175ec3a0ba
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- services/core/java/com/android/server/wm/RootWindowContainer.java
+++ services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -251,7 +251,7 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
     DeviceStateAutoRotateSettingController mDeviceStateAutoRotateSettingController;
 
     // Whether tasks have moved and we need to rank the tasks before next OOM scoring
-    private boolean mTaskLayersChanged = true;
+    boolean mTaskLayersChanged = true;
     private int mTmpTaskLayerRank;
     private final RankTaskLayersRunnable mRankTaskLayersRunnable = new RankTaskLayersRunnable();
     private Region mTmpOccludingRegion;
@@ -2973,7 +2973,9 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
     void invalidateTaskLayers() {
         if (!mTaskLayersChanged) {
             mTaskLayersChanged = true;
-            mService.mH.post(mRankTaskLayersRunnable);
+            if (!mWindowManager.mWindowPlacerLocked.isLayoutDeferred()) {
+                mService.mH.post(mRankTaskLayersRunnable);
+            }
         }
     }
 
