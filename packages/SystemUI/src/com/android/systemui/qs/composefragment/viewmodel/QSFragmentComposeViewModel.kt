de99fde6ae9099cd8e16ab9775bbd39e30757da8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-23 11:06:45 +0800

    [bugfix] avoid using deriveStateOf for media translation
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/viewmodel/QSFragmentComposeViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/viewmodel/QSFragmentComposeViewModel.kt
@@ -339,22 +339,21 @@ constructor(
 
     var shouldUpdateSquishinessOnMedia by mutableStateOf(false)
 
-    val qsMediaTranslationY by derivedStateOf {
-        if (
+    val qsMediaTranslationY: Float
+        get() = if (
             !MediaControlsInComposeFlag.isEnabled &&
-                !Flags.mediaControlsTranslationFix() &&
-                qsExpansion > 0f &&
-                !isKeyguardState &&
-                !qqsMediaVisible &&
-                !qsMediaInRow &&
-                !isInSplitShade
+            !Flags.mediaControlsTranslationFix() &&
+            qsExpansion > 0f &&
+            !isKeyguardState &&
+            !qqsMediaVisible &&
+            !qsMediaInRow &&
+            !isInSplitShade
         ) {
             val interpolation = Interpolators.ACCELERATE.getInterpolation(1f - qsExpansion)
             -qsMediaHost.hostView.height * 1.3f * interpolation
         } else {
             0f
         }
-    }
 
     val animateTilesExpansion: Boolean
         get() = inFirstPage && !mediaSuddenlyAppearingInLandscape
