xiaomi-sm8450-marble/android_frameworks_base_new

Cover more cases when ignoring secure window flags

core/java/android/view/Window.java
@@ -1305,10 +1305,6 @@ public abstract class Window {
      * @see #clearFlags
      */
     public void setFlags(int flags, int mask) {
-        if ((mask & FLAG_SECURE) != 0 && Settings.Secure.getInt(mContext.getContentResolver(),
-                "window_ignore_secure", 0) == 1) {
-            mask &= ~FLAG_SECURE;
-        }
         final WindowManager.LayoutParams attrs = getAttributes();
         attrs.flags = (attrs.flags&~mask) | (flags&mask);
         mForcedWindowFlags |= mask;
@@ -1325,6 +1321,10 @@ public abstract class Window {
      * {@hide}
      */
     protected void dispatchWindowAttributesChanged(WindowManager.LayoutParams attrs) {
+        if ((attrs.flags & FLAG_SECURE) != 0 && Settings.Secure.getInt(mContext.getContentResolver(),
+                "window_ignore_secure", 0) == 1) {
+            attrs.flags &= ~FLAG_SECURE;
+        }
         if (mCallback != null) {
             mCallback.onWindowAttributesChanged(attrs);
         }

peridot-lab/frameworks_base_yaap

core/java/android/view/Window.java
@@ -1368,10 +1368,6 @@ public abstract class Window {
      * @see #clearFlags
      */
     public void setFlags(int flags, int mask) {
-        if ((mask & FLAG_SECURE) != 0 && Settings.Global.getInt(mContext.getContentResolver(),
-                Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1) {
-            mask &= ~FLAG_SECURE;
-        }
         final WindowManager.LayoutParams attrs = getAttributes();
         attrs.flags = (attrs.flags&~mask) | (flags&mask);
         mForcedWindowFlags |= mask;
@@ -1394,6 +1390,10 @@ public abstract class Window {
      * @hide
      */
     protected void dispatchWindowAttributesChanged(WindowManager.LayoutParams attrs) {
+        if ((attrs.flags & FLAG_SECURE) != 0 && Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1) {
+            attrs.flags &= ~FLAG_SECURE;
+        }
         if (mCallback != null) {
             mCallback.onWindowAttributesChanged(attrs);
         }