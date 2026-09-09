c1dc3f2461fb06cab639be555adb5de87f05a98b
grepfox <grepfox@tutamail.com>
2026-09-09 19:23:45 +0000

    SystemUI: add the Dynamic Island size scale settings keys
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8202,6 +8202,16 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String STATUS_BAR_DYNAMIC_ISLAND_WIDTH_SCALE = "status_bar_dynamic_island_width_scale";
+
+        /**
+         * @hide
+         */
+        public static final String STATUS_BAR_DYNAMIC_ISLAND_HEIGHT_SCALE = "status_bar_dynamic_island_height_scale";
+
+        /**
+         * @hide
+         */
         public static final String STATUS_BAR_DYNAMIC_ISLAND_FLASHLIGHT = "status_bar_dynamic_island_flashlight";
 
         /**
