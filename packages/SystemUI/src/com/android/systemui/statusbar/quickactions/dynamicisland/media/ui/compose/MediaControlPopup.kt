7645430a1461d933158b2bddda2b4c9e4c764290
Ghosuto <clash.raja10@gmail.com>
2026-09-09 19:22:30 +0000

    SystemUI: DynamicIsland: Fixup touch handling in squiggly media progress
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/quickactions/dynamicisland/media/ui/compose/MediaControlPopup.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/quickactions/dynamicisland/media/ui/compose/MediaControlPopup.kt
@@ -336,6 +336,7 @@ private fun MediaProgressSection(
                             max = durationMs.toClampedInt()
                             splitTrack = false
                             setPadding(0, 0, 0, 0)
+                            isEnabled = false
                             thumb = createSeekBarThumb(context, accentArgb)
                             thumbOffset = thumb.intrinsicWidth / 2
 
